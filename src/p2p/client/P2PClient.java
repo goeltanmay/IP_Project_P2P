eca2eb778fe3456c35e58cd4e1ff760892ee23d1
Tanmay goel <goeltanmay@gmail.com>
2017-03-08 18:49:13 -0500

    first running client server

--- src/p2p/client/P2PClient.java
+++ src/p2p/client/P2PClient.java
@@ -1,19 +1,34 @@
 package p2p.client;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Iterator;
+import java.util.stream.Stream;
 
 public class P2PClient {
 
 	public static void main(String args[]) throws UnknownHostException, IOException{
 		
 		Socket s = new Socket("127.0.0.1",7734);
+//		s.setKeepAlive(true);
 		PrintStream outputStream = new PrintStream(s.getOutputStream());
-		outputStream.print("Hello server");
-		return;
+		InputStream inputStream = s.getInputStream();
+		InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+		
+		BufferedReader buff = new BufferedReader(inputStreamReader);
+		outputStream.println("Hello server\n");
+		outputStream.flush();
+	
+		
+		System.out.println(buff.readLine());
+		s.close();
 		
 	}
 }
--- src/p2p/server/P2PServer.java
+++ src/p2p/server/P2PServer.java
@@ -1,22 +1,40 @@
 package p2p.server;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Iterator;
+import java.util.stream.Stream;import sun.print.resources.serviceui;
 
 public class P2PServer {
 
 	public static void main(String args[]) throws IOException{
 		System.out.println(" Hello World ");
 		ServerSocket listener = new ServerSocket(7734);
-		Socket s = listener.accept();
-		
-		DataInputStream din = new DataInputStream(s.getInputStream());
-		BufferedReader buff = new BufferedReader(new InputStreamReader(s.getInputStream()));
-		System.out.println(buff.readLine());
-		return;
+		while(true){
+			
+			// code for reading the client's message
+			Socket s = listener.accept();
+			DataInputStream din = new DataInputStream(s.getInputStream());
+			BufferedReader buff = new BufferedReader(new InputStreamReader(din));
+			System.out.println(buff.readLine());
+			System.out.println("here");
+			DataOutputStream outputStream = new DataOutputStream(s.getOutputStream());
+			OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream);
+			BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
+			bufferedWriter.write("got the message\n");
+			bufferedWriter.flush();
+			s.close();
+			
+		}
+//	listener.close();
 	}
 }
