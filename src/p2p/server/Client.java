99ccd8c9124335fca27df0e0f896fdaf54d3d7dd
Tanmay goel <goeltanmay@gmail.com>
2017-03-10 11:15:37 -0500

    Threading work by Akriti

--- src/p2p/server/Client.java
+++ src/p2p/server/Client.java
@@ -1,5 +1,11 @@
 package p2p.server;
 
 public class Client {
+public String hostname;
+public String clientport;
 
+public Client(String hostname, String clientportname){
+	this.hostname= hostname;
+	this.clientport=clientportname;
+}
 }
--- src/p2p/server/P2PServer.java
+++ src/p2p/server/P2PServer.java
@@ -6,6 +6,8 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintStream;
 import java.io.PrintWriter;
@@ -16,32 +18,63 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Stream;import sun.print.resources.serviceui;
 
-public class P2PServer {
+public class P2PServer implements Runnable{
 	public static volatile List<RFC> rfcs;
 	public static volatile List<Client> peers;
-	public static List<ConnectionHandler> threads;
+	ServerSocket listener;
+	//public static List<ConnectionHandler> threads;
 	
 	public static void main(String args[]) throws IOException{
 		rfcs = new LinkedList<>();
 		peers = new LinkedList<>();
+		int port = 7734;
+		 new P2PServer(port);
+		 }
+	public P2PServer(int port) throws IOException{
 		ServerSocket listener = new ServerSocket(7734);
-		while(true){
+		new Thread(this).start();
+		}
+		
 			
 			// code for reading the client's message
-			Socket s = listener.accept();
-			DataInputStream din = new DataInputStream(s.getInputStream());
-			BufferedReader buff = new BufferedReader(new InputStreamReader(din));
-			ClientRequest c = P2PParser.parse(buff.readLine());
+			//Socket s = listener.accept();
+			//DataInputStream din = new DataInputStream(s.getInputStream());
+			//BufferedReader buff = new BufferedReader(new InputStreamReader(din));
+			//ClientRequest c = P2PParser.parse(buff.readLine());
 			
-			System.out.println(c.toString());
-			DataOutputStream outputStream = new DataOutputStream(s.getOutputStream());
-			OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream);
-			BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
-			bufferedWriter.write("got the message\n");
-			bufferedWriter.flush();
-			s.close();
+			//System.out.println(c.toString());
+			//DataOutputStream outputStream = new DataOutputStream(s.getOutputStream());
+			//OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream);
+			//BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
+			//bufferedWriter.write("got the message\n");
+			//bufferedWriter.flush();
+			//s.close();
 			
-		}
+		
 //	listener.close();
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		Socket s;
+		int clientPort=0;
+		String hostname;
+		DataInputStream din;
+		ObjectInputStream input = null;
+		ObjectOutputStream output = null;
+	 try {
+		s= listener.accept();
+		new Thread(this).start();
+		clientPort= s.getPort();
+		input = new ObjectInputStream(s.getInputStream());
+		output= new ObjectOutputStream(s.getOutputStream());
+		String clientportname = Integer.toString(clientPort);
+		hostname = (input.readObject()).toString();
+		peers.add(new Client(hostname,clientportname));
+		System.out.println("Peer Added");
+	} catch (IOException | ClassNotFoundException e) {
+		e.printStackTrace();
+	} 
 	}
-}
+	}
+
